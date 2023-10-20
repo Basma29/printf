3604aee21371b703181459dd2f99f835de64981b
basma29 <basma@gmail.com>
2023-10-20 08:36:33 -0700

    print

--- _printf.c
+++ _printf.c
@@ -1,22 +1,15 @@
 #include "main.h"
+#include<stdio.h>
 /**
  * _printf - produces output according to a format.
- *
  * @format: string
- *
  * Return: the number of characters printed excluding '\0'
  */
 int _printf(const char *format, ...)
 {
-int i = 0;
-int count = 0;
-
 va_list args;
 va_start(args, format);
-while (format[i] != '\0')
-{
-if (format[i] != '%')
-{
-putchar(format[i]);
-count++;
+int count = vprintf(format, args);
+va_end(args);
+return (count);
 }
