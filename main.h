f3db2446b071653d37cbf3e37e2d7400adf036fe
basma29 <basma@gmail.com>
2023-10-20 11:54:19 -0700

    print

--- main.h
+++ main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list c);
@@ -18,14 +15,15 @@ int print_x(va_list x);
 int print_X(va_list X);
 int print_rot13(va_list R);
 /**
-* struct code_format - Struct format
-*
-* @sc: The specifiers
-* @f: The function associated
-*/
+ * struct code_format - Struct format
+ *
+ * @sc: The specifiers
+ * @f: The function associated
+ */
 typedef struct code_format
 {
 char *sc;
 int (*f)(va_list);
 } code_f;
+
 #endif
